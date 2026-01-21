24e1e2ad5a0bc59fd0d902d635ac24e41263ab11
Dev kumar <devkmandal0@gmail.com>
2026-01-21 19:15:19 +0530

    Add db and route for signin

--- backend/dist/index.js
+++ backend/dist/index.js
@@ -1,4 +1,38 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-console.log("hello world");
+import express, {} from 'express';
+import bodyParser from 'body-parser';
+import { config } from 'dotenv';
+import mongoose from 'mongoose';
+import { User } from './db.js';
+const app = express();
+config();
+app.use(bodyParser.json());
+const MONGO_URL = process.env.MONGO_URL;
+app.post('/signin', async (req, res) => {
+    const { name, email, password } = req.body;
+    try {
+        const findUser = await User.findOne({
+            email: email,
+        });
+        if (!findUser) {
+            const response = await User.create([
+                {
+                    name: name,
+                    email: email,
+                    password: password
+                }
+            ]);
+            res.status(200).json(response);
+        }
+        else {
+            res.status(400).json("User already exist");
+        }
+    }
+    catch (error) {
+        console.log(error);
+    }
+});
+mongoose.connect("mongodb://127.0.0.1:27017/paytm?directConnection=true&serverSelectionTimeoutMS=2000&appName=mongosh+2.6.0").then(() => {
+    console.log('db connected');
+    app.listen(3000, () => console.log("Server is listen on -", 3000));
+}).catch((error) => console.log(error));
 //# sourceMappingURL=index.js.map
